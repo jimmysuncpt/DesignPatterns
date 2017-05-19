6ab43597cdc77ac6c9de5efee72289e1c7fd3020
Jimmy Sun <jimmysuncpt@gmail.com>
2017-05-19 10:13:52 +0800

    建立接口

---
+++ src/com/jimmysun/designpatterns/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.jimmysun.designpatterns.observer;
+
+public interface DisplayElement {
+	public void display();
+}
---
+++ src/com/jimmysun/designpatterns/observer/Observer.java
@@ -0,0 +1,5 @@
+package com.jimmysun.designpatterns.observer;
+
+public interface Observer {
+	public void update(float temp, float humidity, float pressure);
+}
