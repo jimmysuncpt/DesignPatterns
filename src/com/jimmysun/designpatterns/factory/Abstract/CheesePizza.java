aad4c32625431380e17fb8037ae732042949bdb1
Jimmy Sun <jimmysuncpt@gmail.com>
2017-05-25 15:45:11 +0800

    不同类型的比萨

---
+++ src/com/jimmysun/designpatterns/factory/Abstract/CheesePizza.java
@@ -0,0 +1,17 @@
+package com.jimmysun.designpatterns.factory.Abstract;
+
+public class CheesePizza extends Pizza {
+	PizzaIngredientFactory ingredientFactory;
+
+	public CheesePizza(PizzaIngredientFactory ingredientFactory) {
+		this.ingredientFactory = ingredientFactory;
+	}
+
+	@Override
+	void prepare() {
+		System.out.println("Preparing " + name);
+		dough = ingredientFactory.createDough();
+		sauce = ingredientFactory.createSauce();
+		cheese = ingredientFactory.createCheese();
+	}
+}
