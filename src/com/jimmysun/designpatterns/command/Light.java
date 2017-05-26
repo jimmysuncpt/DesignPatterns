0584dc39126a287cde85b301fb8971d910a5e4a8
Jimmy Sun <jimmysuncpt@gmail.com>
2017-05-26 21:17:53 +0800

    电灯

---
+++ src/com/jimmysun/designpatterns/command/Light.java
@@ -0,0 +1,15 @@
+package com.jimmysun.designpatterns.command;
+
+public class Light {
+
+	public Light() {
+	}
+
+	public void on() {
+		System.out.println("Light is on");
+	}
+
+	public void off() {
+		System.out.println("Light is off");
+	}
+}
