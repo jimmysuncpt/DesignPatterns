d9b0a7174c14927556192902d38fc9cafd963a16
Jimmy Sun <jimmysuncpt@gmail.com>
2017-05-31 16:36:48 +0800

    电灯开命令

---
+++ src/com/jimmysun/designpatterns/command/undo/LightOnCommand.java
@@ -0,0 +1,21 @@
+package com.jimmysun.designpatterns.command.undo;
+
+import com.jimmysun.designpatterns.command.Light;
+
+public class LightOnCommand implements Command {
+	Light light;
+
+	public LightOnCommand(Light light) {
+		this.light = light;
+	}
+
+	@Override
+	public void execute() {
+		light.on();
+	}
+
+	@Override
+	public void undo() {
+		light.off();
+	}
+}
