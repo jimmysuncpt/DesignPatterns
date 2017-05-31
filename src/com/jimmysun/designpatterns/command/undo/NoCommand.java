c35b600408d07c33c18b440fea32fb569344f2dd
Jimmy Sun <jimmysuncpt@gmail.com>
2017-05-31 16:37:09 +0800

    无操作命令

---
+++ src/com/jimmysun/designpatterns/command/undo/NoCommand.java
@@ -0,0 +1,11 @@
+package com.jimmysun.designpatterns.command.undo;
+
+public class NoCommand implements Command {
+	@Override
+	public void execute() {
+	}
+
+	@Override
+	public void undo() {
+	}
+}
